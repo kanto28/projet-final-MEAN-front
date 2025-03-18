55580f10221da63fa6618fafb7b8679a3a986c38
kanto28 <kantoandrianaivosoa28@gmail.com>
2025-03-18 10:13:44 +0300

    update service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,17 +7,4 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:5001'; // Remplacez par l'URL de votre backend
-
-  constructor(private http: HttpClient) { }
-
-  // Méthode pour l'inscription
-  register(userData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/formulaire`, userData);
-  }
-
-  // Méthode pour la connexion
-  login(credentials: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, credentials); // Remplacez par l'endpoint de connexion de votre backend
-  }
 }
