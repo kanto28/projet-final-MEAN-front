306888874db13f31b14567c7af890f9b55095cc0
kanto28 <kantoandrianaivosoa28@gmail.com>
2025-03-19 18:41:39 +0300

    Type de vehicule crud

--- src/app/pages/crud/type-vehicule/type-vehicule.component.ts
+++ src/app/pages/crud/type-vehicule/type-vehicule.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -17,6 +17,7 @@ import { TagModule } from 'primeng/tag';
 import { TextareaModule } from 'primeng/textarea';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TypeVehiculeService } from '../../../services/crud/typeVehicule/type-vehicule.service';
 
 @Component({
   selector: 'app-type-vehicule',
@@ -43,44 +44,113 @@ import { ToolbarModule } from 'primeng/toolbar';
   templateUrl: './type-vehicule.component.html',
   styleUrl: './type-vehicule.component.scss'
 })
-export class TypeVehiculeComponent {
-  ajoutTypeVehicule: boolean = false;
-  editTypeVehicule: boolean = false;
-  submitted: boolean = false;
-  displayConfirmation: boolean = false;
+export class TypeVehiculeComponent implements OnInit{
+
+  typeVehiculeName: string = '';
+  typeVehicules: any[] = [];
+  selectedTypeVehicule: any = null; 
+  editTypeVehiculeDialog: boolean = false; 
+  typeVehiculeToDelete: any = null;
+
+  constructor(private typeVehiculeService: TypeVehiculeService) { }
+
+   // Creer une nouvelle Type de Vehicule
+   onCreateTypeVehicule() {
+    this.typeVehiculeService.createMoteur(this.typeVehiculeName).subscribe(
+      response => {
+        console.log('Type de Vehicule  créée avec succès', response);
+      },
+      error => {
+        console.error('Erreur lors de la création du Type de Vehicule', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.loadTypeVehicule(); 
+  }
   
-
-  products = [
-    { id: '01', name: 'Toyota', price: 20000, category: 'SUV', reviews: 4, status: 'Available' },
-    { id: '02', name: 'Ford', price: 25000, category: 'Truck', reviews: 5, status: 'Sold Out' },
-    { id: '03', name: 'BMW', price: 30000, category: 'Sedan', reviews: 4.5, status: 'Available' }
-  ];
-
-  ovrirNouveauTypeVehicule() {
-    this.submitted = false;
-    this.ajoutTypeVehicule = true;
+  loadTypeVehicule(): void {
+    this.typeVehiculeService.getTypeVehicules().subscribe(
+      (data) => {
+        this.typeVehicules = data;
+        console.log('Type de Vehicule récupérées :', this.typeVehicules);  
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des type de vehicule', error);
+      }
+    );
   }
 
-  hideDialog() {
-    this.ajoutTypeVehicule = false;
-    this.submitted = false;
+    ajoutTypeVehicule: boolean = false;
+    submitted: boolean = false;
+    displayConfirmation: boolean = false;
+
+    ovrirNouveauTypeVehicule() {
+      this.submitted = false;
+      this.ajoutTypeVehicule = true;
+    }
+  
+  
+    hideAjoutTypeVehicule() {
+      this.ajoutTypeVehicule = false;
+    }
+
+    // open update
+  openEditTypeVehiculeDialog(typeVehicule: any) {
+    this.selectedTypeVehicule = { ...typeVehicule }; 
+    this.editTypeVehiculeDialog = true;
+  }
+  
+  // close modal updaate
+  hideEditTypeVehiculeDialog() {
+    this.editTypeVehiculeDialog = false;
+    this.selectedTypeVehicule = null;
   }
 
-  ovrirEditTypeVehicule() {
-    this.submitted = false;
-    this.editTypeVehicule = true;
+  // creer type de vehicule
+  onUpdateTypeVehicule() {
+    if (this.selectedTypeVehicule) {
+      this.typeVehiculeService.updateTypeVehicule(this.selectedTypeVehicule._id, this.selectedTypeVehicule.name).subscribe(
+        response => {
+          console.log('Type de Vehicule mise à jour avec succès', response);
+          this.loadTypeVehicule(); 
+          this.hideEditTypeVehiculeDialog(); 
+        },
+        error => {
+          console.error('Erreur lors de la mise à jour du type de vehicule', error);
+        }
+      );
+    }
   }
 
-  hideEditTypeVehicule() {
-    this.editTypeVehicule = false;
-    this.submitted = false;
+  // modal supp
+  openConfirmation(typeVehicule: any) {
+    this.typeVehiculeToDelete = typeVehicule; 
+    this.displayConfirmation = true; 
+  }
+  
+  // close modal supp
+  closeConfirmation() {
+    this.displayConfirmation = false;
+    this.typeVehiculeToDelete = null; 
   }
 
-  openConfirmation() {
-    this.displayConfirmation = true;
+  //supprimer l'Type de vehicule
+  onDeleteTypeVehicule() {
+    if (this.typeVehiculeToDelete) {
+      this.typeVehiculeService.deleteTypeVehicule(this.typeVehiculeToDelete._id).subscribe(
+        response => {
+          console.log('type de vehicule supprimée avec succès', response);
+          this.loadTypeVehicule(); 
+          this.closeConfirmation();
+        },
+        error => {
+          console.error('Erreur lors de la suppression du type de vehicule', error);
+        }
+      );
+    }
+  }
+
 }
 
-closeConfirmation() {
-  this.displayConfirmation = false;
-}
-}
---
+++ src/app/services/crud/typeVehicule/type-vehicule.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypeVehiculeService {
+
+   private apiUrl = 'http://localhost:5001/api/typevehicule/typevehicules';
+   
+     constructor(private http: HttpClient) { }
+   
+     //créer une nouvelle moteurs
+     createMoteur(name: string): Observable<any> {
+       const token = localStorage.getItem('token'); 
+       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); 
+       return this.http.post(this.apiUrl, { name }, { headers });
+     }
+     
+   
+     // Récupérer toutes les type de vehicules
+     getTypeVehicules(): Observable<any> {
+       return this.http.get<any>(this.apiUrl);
+     }
+   
+     // Mettre à jour une type de vehicules existante
+   updateTypeVehicule(id: string, name: string): Observable<any> {
+     const token = localStorage.getItem('token'); 
+     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); 
+     const url = `${this.apiUrl}/${id}`; 
+     return this.http.put(url, { name }, { headers });
+   }
+   
+   // Supprimer une type de vehicules existante
+   deleteTypeVehicule(id: string): Observable<any> {
+     const token = localStorage.getItem('token'); 
+     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); 
+     const url = `${this.apiUrl}/${id}`; 
+     return this.http.delete(url, { headers });
+   }
+}
